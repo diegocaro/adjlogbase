b5fb82926ca60edd8155c4c4746639c7d43eeb09
Diego Caro <diegocaro@gmail.com>
2013-09-01 01:17:02 -0400

    solving a bug

--- tgraph.cpp
+++ tgraph.cpp
@@ -121,7 +121,11 @@ void TGraph::create(TGraphReader &tgr) {
                 if (i%1000==0) fprintf(stderr, "Compressing: %0.2f%%\r", (float)i*100/nodes);
                 node_changes = tgr.tgraph[i].changes;
                 
-                if (node_changes == 0) { continue;}
+                tgraph[i].changes = 0;
+                tgraph[i].csize_time = 0;
+                tgraph[i].csize_neighbors = 0;
+                
+                if (node_changes == 0) { LOG("node %u with zero changes\n") ;continue;}
                  
                 csize_neigh = etdc_encode(&table, tgr.tgraph[i].neighbors.data(), node_changes, ccnodesbuffer);
                 tgraph[i].cneighbors = new unsigned char [csize_neigh];
